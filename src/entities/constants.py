6a0180b74e5be86b3feded3d915786cd90a2d063
Toprak <toprak.bilici@gmail.com>
2024-07-07 02:26:19 +0300

    .gitignore update

--- src/entities/constants.py
+++
@@ -1,8 +0,0 @@
-
-
-class Constants:
-    dot_env_file_path = "/.env"
-    ca_cert_file_path = "/home/djentci/http_ca.crt"
-    es_port_url = "https://localhost:9200"
-    bulk_data_file_name = "products-bulk (1).json"
-    index_name = "products"
